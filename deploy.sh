28a990c99fbadc4fc16fc082cfcb652fa30884c9
Sunjin Yang <lethean@emstone.com>
2020-02-13 16:54:11 +0900

    deploy: set cache max-age to 7 days

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 [ "$1" = "clean" ] && rm -rf public
 hugo
 
-hugo deploy --target=emstone.nvrsw.com
+hugo deploy --target=emstone.nvrsw.com --maxDeletes -1
 exit $?
 
 aws s3 sync public s3://emstone.nvrsw.com/ --delete
