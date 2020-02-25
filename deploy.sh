f9a9c4e743db1ca05d881b067e0733ba03df9e10
Sunjin Yang <lethean@emstone.com>
2020-02-25 15:25:46 +0900

    deploy: invalidate CloudFront cache after uploading

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@
 hugo --minify
 
 hugo deploy --target=www.emstone.com --maxDeletes -1
+aws cloudfront create-invalidation --distribution-id=E25OIC4F6N9LOY --paths "/*"
 exit $?
 
 aws s3 sync public s3://www.emstone.com/ --delete
