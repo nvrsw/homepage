027e75c84119060b56613ea4677d48dd005294e7
Sunjin Yang <lethean@emstone.com>
2020-02-25 12:49:11 +0900

    deploy: deploy to https://www.emstone.com/

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 [ "$1" = "clean" ] && rm -rf public
 hugo --minify
 
-hugo deploy --target=emstone.nvrsw.com --maxDeletes -1
+hugo deploy --target=www.emstone.com --maxDeletes -1
 exit $?
 
-aws s3 sync public s3://emstone.nvrsw.com/ --delete
+aws s3 sync public s3://www.emstone.com/ --delete
