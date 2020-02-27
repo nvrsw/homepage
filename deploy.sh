24ff2d925c1fd6b5c910a3b093b776c9279ad69d
Sunjin Yang <lethean@emstone.com>
2020-02-27 16:35:27 +0900

    deploy: add software update only

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-[ "$1" = "clean" ] && rm -rf public
+[ "$1" = "clean" ] && rm -rf public && shift
 hugo --minify
 
+INVALIDATE_PATH="/*"
+[ "$1" = "software" ] && INVALIDATE_PATH="/support/software/*" && shift
+[ -n "$1" ] && INVALIDATE_PATH="$1"
+
 hugo deploy --target=www.emstone.com --maxDeletes -1
-aws cloudfront create-invalidation --distribution-id=E25OIC4F6N9LOY --paths "/*"
+aws cloudfront create-invalidation --distribution-id=E25OIC4F6N9LOY --paths "$INVALIDATE_PATH"
 exit $?
 
 aws s3 sync public s3://www.emstone.com/ --delete
