a7f3c39fa24b91f3946af9ddbfdb1ec34e7d03ff
Sunjin Yang <lethean@emstone.com>
2020-03-05 10:29:26 +0900

    deploy: forward browsers to pages for each language

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,25 @@
 [ "$1" = "clean" ] && rm -rf public && shift
 hugo --minify
 
+# Update home index file to check browser language code.
+cat << _EOF > public/index.html
+<!doctype html>
+<html><head>
+<title>EMSTONE</title>
+<link rel=canonical href=https://www.emstone.com/ko>
+<meta name=robots content="noindex">
+<meta charset=utf-8>
+<meta http-equiv=refresh content="1; url=https://www.emstone.com/ko">
+<script>
+var lang = navigator.language || navigator.userLanguage; 
+if (lang.slice(0, 2) == 'ko')
+  window.location.replace('https://www.emstone.com/ko');
+else
+  window.location.replace('https://www.emstone.com/en');
+</script>
+</head></html>
+_EOF
+
 INVALIDATE_PATH="/*"
 [ "$1" = "software" ] && INVALIDATE_PATH="/support/software/*" && shift
 [ -n "$1" ] && INVALIDATE_PATH="$1"
