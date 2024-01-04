fa251b641cb4fe9f588a62b4f79b045dccf3a777
Sunjin Yang <lethean@emstone.com>
2024-01-04 12:24:09 +0900

    deploy: update icon links

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,13 @@ set -xe
 
 hugo --minify
 
-# Update home index file to check browser language code.
-cp -f assets/index.html public/
+# Update home index file to check browser language code and update icon links.
+grep -o -E -e '<link rel="?(manifest|icon|apple-touch-icon)[ "0-9a-z=/_\.]*>' public/en/index.html |
+sed -e '/<!--icon-link-->/{r /dev/stdin' -e 'd;}' assets/index.html > public/index.html
 
 # Let Korean RSS feeds be global RSS feeds.
 cp -f public/ko/index.xml public/
 cp -f public/ko/index.json public/
-cp -f public/ko/manifest.webmanifest public/
 
 hugo deploy --maxDeletes -1 --invalidateCDN
 
